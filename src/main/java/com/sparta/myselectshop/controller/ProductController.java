f69147c6c7a3c48fe2ef497bcbbb70e2768172de
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-06-29 18:05:23 +0900

    회원별 상품 API 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -3,8 +3,10 @@ package com.sparta.myselectshop.controller;
 import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.security.UserDetailsImpl;
 import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,9 +17,15 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+
+    /*
+     *  관심 상품 등록
+     *  @Param: ProductRequestDto(제품정보 전달), UserDetailsImpl(회원정보 전달)
+     *  @return: productResponseDto
+     */
     @PostMapping("/products")
-    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
-        return productService.createProduct(requestDto);
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return productService.createProduct(requestDto,userDetails.getUser());
     }
 
     @PutMapping("/products/{id}")
@@ -25,8 +33,22 @@ public class ProductController {
         return productService.updateProduct(id,requestDto);
     }
 
+    /*
+     *  관심 상품 조희
+     *  @Param: UserDetailsImpl(회원정보 전달)
+     *  @return: List<ProductResponseDto> : 유저가 등록한 모든 상품들
+     */
     @GetMapping("/products")
-    public List<ProductResponseDto> getProducts(){
-        return productService.getProducts();
+    public List<ProductResponseDto> getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        return productService.getProducts(userDetails.getUser());
+    }
+
+    /*
+     *  전체상품 조희(어드민 전용)
+     *  @return: List<ProductResponseDto> : 모든유저가 등록한 모든 상품들
+     */
+    @GetMapping("/admin/products")
+    public List<ProductResponseDto> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
